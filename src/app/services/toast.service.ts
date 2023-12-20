3a6faa8db68efbd705667f1dbcce173e78104b66
jesakim <jesakim1@gmail.com>
2023-12-20 09:19:42 +0100

    adding some functionalities

--- src/app/services/toast.service.ts
+++ src/app/services/toast.service.ts
@@ -10,7 +10,7 @@ export class ToastService {
   showSuccess(message: string): void {
     const toast = document.createElement('div');
     toast.className = 'toast show';
-    toast.innerHTML = `<div class="toast-header " style="background-color: #0c151e; color: #f9eeb9;>
+    toast.innerHTML = `<div class="toast-header ">
                           <strong class="me-auto">Alert</strong>
                           <button type="button" class="btn-close text-white" data-bs-dismiss="toast" aria-label="Close"></button>
                         </div>
@@ -23,7 +23,7 @@ export class ToastService {
 
       const bsToast = new (bootstrap.Toast as any)(toast);
       bsToast.show();
-  
+
       bsToast.addEventListener('hidden.bs.toast', () => {
         toastContainer.removeChild(toast);
       });
